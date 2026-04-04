6363c8a48c2d3c738140d8f036754dcf8782d985
Vilizar Denichin <denichinv@gmail.com>
2026-04-04 16:12:25 +0100

    feat: add Navbar component for navigation between pages

--- ai-finance-app/client/src/App.tsx
+++ ai-finance-app/client/src/App.tsx
@@ -2,11 +2,13 @@ import Dashboard from "./pages/Dashboard";
 import AddTransaction from "./pages/AddTransaction";
 import Goals from "./pages/Goals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./layout/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="add" element={<AddTransaction />} />
