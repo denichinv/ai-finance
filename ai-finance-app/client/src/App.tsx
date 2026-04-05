4b80504ba7e8df125a9a4b84bc59726405e921f0
Vilizar Denichin <denichinv@gmail.com>
2026-04-05 17:01:24 +0100

    feat: add NotFound component and update routing in App to handle 404 errors

--- ai-finance-app/client/src/App.tsx
+++ ai-finance-app/client/src/App.tsx
@@ -3,6 +3,7 @@ import AddTransaction from "./pages/AddTransaction";
 import Goals from "./pages/Goals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="add" element={<AddTransaction />} />
           <Route path="goals" element={<Goals />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
