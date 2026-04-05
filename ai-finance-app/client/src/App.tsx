07df68eb89fc97195c0a8fb4d79876b0c3d89c1c
Vilizar Denichin <denichinv@gmail.com>
2026-04-05 17:29:45 +0100

    feat: enhance AddTransaction component with transaction handling and form validation

--- ai-finance-app/client/src/App.tsx
+++ ai-finance-app/client/src/App.tsx
@@ -4,19 +4,38 @@ import Goals from "./pages/Goals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import NotFound from "./pages/NotFound";
+import type { Transaction } from "./types/transaction";
+import { useState } from "react";
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const handleTransaction = (newTransaction: Transaction) => {
+    setTransactions((prev) => [...prev, newTransaction]);
+  };
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="add" element={<AddTransaction />} />
+          <Route
+            path="add"
+            element={<AddTransaction onAddTransaction={handleTransaction} />}
+          />
           <Route path="goals" element={<Goals />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      {transactions.length === 0 ? (
+        <p>No transactions yet</p>
+      ) : (
+        transactions.map((t, i) => (
+          <div key={i}>
+            {t.category} - £{t.amount} - {t.date}
+          </div>
+        ))
+      )}
     </>
   );
 }
