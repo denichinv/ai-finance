6370e38aa93803f80d611f160b4910d02aff18a2
Vilizar Denichin <denichinv@gmail.com>
2026-04-15 03:47:14 +0100

    add basic UI animations with framer motion (page load and buttons)

--- ai-finance-app/client/src/pages/AddTransaction.tsx
+++ ai-finance-app/client/src/pages/AddTransaction.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { Transaction } from "../types/transaction";
+import { motion } from "framer-motion";
 
 type Props = {
   onAddTransaction: (Transaction: Transaction) => void;
@@ -34,67 +35,77 @@ export default function AddTransaction({ onAddTransaction }: Props) {
   };
 
   return (
-    <div className="max-w-xl mx-auto">
-      {/* Card */}
-      <div className="bg-white p-6 rounded-xl  shadow-sm">
-        {/*title and description */}
-        <h1 className="text-xl font-semibold text-gray-900 mb-2">
-          Add Transaction
-        </h1>
-        <p className="text-gray-500 text-sm mb-6">
-          Use this form to add a new transaction to your financial records. You
-          can specify the amount, date, category, and description of the
-          transaction.
-        </p>
-
-        {/* Form */}
-
-        <form onSubmit={handleSubmit} className="space-y-4">
-          {/*Amount*/}
-
-          <label className="block text-sm text-gray-600 mb-1"> Amount:</label>
-          <input
-            className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-
-          {/*Category*/}
-          <label className="block text-sm text-gray-600 mb-1"> Category:</label>
-          <input
-            className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
-            type="text"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-          {/*Type*/}
-          <label className="block text-sm text-gray-600 mb-1"> Type:</label>
-          <select
-            value={type}
-            onChange={(e) => setType(e.target.value as "income" | "expense")}
-            className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
-          >
-            <option value="expense">Expense</option>
-            <option value="income">Income</option>
-          </select>
-          {/*Date*/}
-          <label className="block text-sm text-gray-600 mb-1"> Date:</label>
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
-          />
-          {/* Button*/}
-          <button
-            type="submit"
-            className="w-full bg-lime-400 text-black font-medium py-2 rounded-lg hover:bg-lime-300 transition"
-          >
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="max-w-xl mx-auto">
+        {/* Card */}
+        <div className="bg-white p-6 rounded-xl  shadow-sm">
+          {/*title and description */}
+          <h1 className="text-xl font-semibold text-gray-900 mb-2">
             Add Transaction
-          </button>
-        </form>
+          </h1>
+          <p className="text-gray-500 text-sm mb-6">
+            Use this form to add a new transaction to your financial records.
+            You can specify the amount, date, category, and description of the
+            transaction.
+          </p>
+
+          {/* Form */}
+
+          <form onSubmit={handleSubmit} className="space-y-4">
+            {/*Amount*/}
+
+            <label className="block text-sm text-gray-600 mb-1"> Amount:</label>
+            <input
+              className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
+              type="number"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+
+            {/*Category*/}
+            <label className="block text-sm text-gray-600 mb-1">
+              {" "}
+              Category:
+            </label>
+            <input
+              className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
+              type="text"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            {/*Type*/}
+            <label className="block text-sm text-gray-600 mb-1"> Type:</label>
+            <select
+              value={type}
+              onChange={(e) => setType(e.target.value as "income" | "expense")}
+              className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
+            >
+              <option value="expense">Expense</option>
+              <option value="income">Income</option>
+            </select>
+            {/*Date*/}
+            <label className="block text-sm text-gray-600 mb-1"> Date:</label>
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className="w-full border border-gray-200 rounded-lg px-3  py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
+            />
+            {/* Button*/}
+            <motion.button
+              whileTap={{ scale: 0.95 }}
+              whileHover={{ scale: 1.02 }}
+              className="w-full bg-lime-400 text-black py-2 rounded-lg"
+            >
+              Add Transaction
+            </motion.button>
+          </form>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
--- ai-finance-app/client/src/pages/NotFound.tsx
+++ ai-finance-app/client/src/pages/NotFound.tsx
@@ -1,22 +1,30 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center min-h-[80vh]">
-      <div className="text-center max-w-md">
-        <h1 className="text-6xl font-bold text-gray-900 mb-2">404</h1>
-        <h2 className="text-xl font-semibold text-gray-800 mb-3">
-          Page not found
-        </h2>
-        <p className="tex-gray-500 mb-6 ">
-          The page you are looking for does not exist.
-        </p>
-        <Link
-          to={"/"}
-          className="w-full bg-lime-400 text-black font-medium py-3 px-6 rounded-lg hover:bg-lime-300 transition"
-        >
-          Go back to Dashboard
-        </Link>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="flex items-center justify-center min-h-[80vh]">
+        <div className="text-center max-w-md">
+          <h1 className="text-6xl font-bold text-gray-900 mb-2">404</h1>
+          <h2 className="text-xl font-semibold text-gray-800 mb-3">
+            Page not found
+          </h2>
+          <p className="tex-gray-500 mb-6 ">
+            The page you are looking for does not exist.
+          </p>
+          <Link
+            to={"/"}
+            className="w-full bg-lime-400 text-black font-medium py-3 px-6 rounded-lg hover:bg-lime-300 transition"
+          >
+            Go back to Dashboard
+          </Link>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
